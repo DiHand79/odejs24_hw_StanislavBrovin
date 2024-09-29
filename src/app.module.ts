fe47e1e7f7cf9c65ee151d91483f998a9825e38a
Stanislav Brovin <stb@sekai.io>
2024-09-29 18:43:17 +0300

    Homework 2 - add middleware as loger IP

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoggerModule } from './logger/logger.module';
+import { MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { LoggerMiddleware } from './logger/logger.middleware';
+
+@Module({
+  imports: [LoggerModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '/', method: RequestMethod.ALL });
+  }
+}
